d46ab10813275eab868ce0358592734555233d95
Julia Morelli <jmlanghe@gmail.com>
2018-11-22 16:28:50 -0300

    Add post creation

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
-    post = Post.objects.get(pk=pk)
+    post = Post.objects.get_object_or_404(pk=pk)
     return render(request, 'post_detail.html', {'post': post})
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'post_edit.html', {'form': form})
